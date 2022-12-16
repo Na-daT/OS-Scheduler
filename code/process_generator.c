24c9a83907364d5223b73cc78cbd610c736ff80b
Na-daT <nadatarek2710@gmail.com>
2022-12-16 15:21:27 +0200

    read file

--- code/process_generator.c
+++ code/process_generator.c
@@ -1,17 +1,46 @@
 #include "headers.h"
+#include "priority_queue.c"
+#include "circular_queue.c"
 
 void clearResources(int);
 
 int main(int argc, char *argv[])
 {
+    if (argc != 2)
+        return fprintf(stderr, "Incorrect input\n");
+
     signal(SIGINT, clearResources);
     // TODO Initialization
-    // 1. Read the input files.
+
+    // 1. Read the input file processes.txt and get the processes and their parameters.
+    FILE *inFile = fopen("processes.txt", "r");
+    if (inFile == NULL)
+    {
+        perror("Error opening processes file");
+        exit(-1);
+    }
+
+    char *line;
+    size_t len, numberOfProcesses = 0;
+    circular_queue *processQueue = create_circular_queue(sizeof(Process));
+    while (getline(&line, &len, inFile) != -1)
+        if (line[0] != '#')
+        {
+            numberOfProcesses++;
+
+            Process *p = malloc(sizeof(*p));
+            sscanf(line, processFormatString, &p->id, &p->arrivaltime, &p->remainingtime, &p->priority);
+
+            enqueueCQ(processQueue, p);
+        }
+
+    fclose(inFile);
+
     // 2. Read the chosen scheduling algorithm and its parameters, if there are any from the argument list.
     // 3. Initiate and create the scheduler and clock processes.
     // 4. Use this function after creating the clock process to initialize clock.
     initClk();
-    // To get time use this function. 
+    // To get time use this function.
     int x = getClk();
     printf("Current Time is %d\n", x);
     // TODO Generation Main Loop
@@ -23,5 +52,5 @@ int main(int argc, char *argv[])
 
 void clearResources(int signum)
 {
-    //TODO Clears all resources in case of interruption
+    // TODO Clears all resources in case of interruption
 }
